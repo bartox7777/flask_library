5445d5163309ff06972aedfa6d8bdc996e2016d5
bartosz <bartosz.zelek@poczta.onet.pl>
2021-05-29 23:04:48 +0200

    Added some extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,25 @@
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+
 from config import config
+
+
+db = SQLAlchemy()
+bootstrap = Bootstrap()
+
+login_manager = LoginManager()
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     # config_name[config_name].init_app(app)
+
+    db.init_app(app)
+    bootstrap.init_app(app)
+    login_manager.init_app(app)
 
     return app
--- app/db.py
+++
@@ -1,8 +0,0 @@
-from flask import g
-from flask_sqlalchemy import SQLAlchemy
-
-
-def get_db():
-    pass
-    # if "db" not in g:
-    #     g.db = SQLAlchemy
