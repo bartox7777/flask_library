d672d1a8476be4ab6a07f2f579ae9adea2ba1099
Bartosz Z <bartosz.zelek@protonmail.com>
2021-06-14 15:30:44 +0200

    Small fixes

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms import SubmitField
-from wtforms.validators import DataRequired
 
 
 class SearchForm(FlaskForm):
-    phrase = StringField("Wyszukaj książkę...", validators=[DataRequired()])
+    phrase = StringField("Wyszukaj książkę...")
     submit = SubmitField("Szukaj")
