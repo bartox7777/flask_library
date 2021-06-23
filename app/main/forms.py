0f5c98c86f68e59300466901546239e8e8ef24a0
bartox7777 <bartosz.zelek@protonmail.com>
2021-06-23 22:05:13 +0200

    Added select field to search form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms import SubmitField
-
-
+from wtforms import SelectField
 class SearchForm(FlaskForm):
     phrase = StringField("Wyszukaj książkę...")
+    search_by = SelectField(choices=["Tytuł", "Gatunek", "Autor"])
     submit = SubmitField("Szukaj")
