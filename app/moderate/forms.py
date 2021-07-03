c0b9081c220a5974f81ad4183182956ab956ff84
Bartosz Z <bartosz.zelek@protonmail.com>
2021-07-03 15:04:01 +0200

    Add book form creating

--- app/moderate/forms.py
+++ app/moderate/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from flask_wtf.recaptcha import validators
 from wtforms import StringField
 from wtforms import SubmitField
 from wtforms import IntegerField
 from wtforms import FileField
 from wtforms import TextAreaField
 from wtforms.validators import DataRequired
+from wtforms.validators import NumberRange
 
 
 class AddBookForm(FlaskForm):
@@ -14,9 +14,9 @@ class AddBookForm(FlaskForm):
     # category = None
     description = TextAreaField("Opis")
     # author = None
-    number_of_copies = IntegerField("Liczba kopii", validators=[DataRequired()])
+    number_of_copies = IntegerField("Liczba kopii", validators=[DataRequired(), NumberRange(min=0)])
     cover = FileField("Okładka")
     # publisher = None
-    pages = IntegerField("Stron", validators=[DataRequired()])
+    pages = IntegerField("Stron", validators=[DataRequired(), NumberRange(min=0)])
     year = IntegerField("Rok wydania", validators=[DataRequired()])
     submit = SubmitField("Dodaj książkę")
--- app/moderate/views.py
+++ app/moderate/views.py
@@ -1,13 +1,16 @@
 from flask import render_template
-from ..main.forms import SearchForm
+from .forms import AddBookForm
 
 from . import moderate
 
 
 @moderate.route("/add-book", methods=("GET", "POST"))
 def add_book():
+    form = AddBookForm()
+
     return render_template(
         "moderate/add_book.html",
         title="Dodaj książkę",
-        dont_show_search_bar=True
+        dont_show_search_bar=True,
+        form=form
     )
