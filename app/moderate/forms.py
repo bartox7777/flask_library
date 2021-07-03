6034a510b24c929656777ef40d112a773126e325
bartox7777 <bartosz.zelek@protonmail.com>
2021-07-03 16:51:57 +0200

    In process

--- app/moderate/forms.py
+++ app/moderate/forms.py
@@ -4,14 +4,25 @@ from wtforms import SubmitField
 from wtforms import IntegerField
 from wtforms import FileField
 from wtforms import TextAreaField
+from wtforms import SelectField
 from wtforms.validators import DataRequired
 from wtforms.validators import NumberRange
 
+from ..models import Book
+
 
 class AddBookForm(FlaskForm):
+    # def __init__(self, session):
+    #     super().__init__()
+    #     self.session = session
+
+    # def fetch_categories(self):
+    #     return [(category[0], category[0]) for category in self.session.query(Book.category).distinct().all()]
+
     isbn = StringField("ISBN", validators=[DataRequired()])
     title = StringField("Tytuł", validators=[DataRequired()])
-    # category = None
+    category = SelectField()
+    # category_add = StringField("Dodaj kategorię")
     description = TextAreaField("Opis")
     # author = None
     number_of_copies = IntegerField("Liczba kopii", validators=[DataRequired(), NumberRange(min=0)])
--- app/moderate/views.py
+++ app/moderate/views.py
@@ -2,11 +2,14 @@ from flask import render_template
 from .forms import AddBookForm
 
 from . import moderate
+from .. import db
+from ..models import Book
 
 
 @moderate.route("/add-book", methods=("GET", "POST"))
 def add_book():
     form = AddBookForm()
+    form.category.choices = [(category[0], category[0]) for category in db.session.query(Book.category).distinct().all()]
 
     return render_template(
         "moderate/add_book.html",
