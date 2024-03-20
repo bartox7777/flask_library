66907e2fff7c465367a34ef9cdd53315a347ae23
bartox7777 <bartosz.zelek@protonmail.com>
2024-03-20 11:33:29 +0100

    create_app()

--- app/__init__.py
+++ app/__init__.py
@@ -16,17 +16,18 @@ babel = Babel()
 migrate = Migrate()
 mail = Mail()
 
+
 # is it good solution?
 class AnonymousUser(AnonymousUserMixin):
-    role = dict(
-        name="anonymous"
-    )
+    role = dict(name="anonymous")
     # TODO: role = Role.query.filter_by(name="anonymous").first()
+
 
 login_manager.login_view = "auth.login"
 login_manager.login_message = "Zaloguj się, aby mieć dostęp do tej strony."
 login_manager.login_message_category = "warning"
 login_manager.anonymous_user = AnonymousUser
+
 
 def create_app(config_name="production"):
     app = Flask(__name__)
@@ -40,6 +41,7 @@ def create_app(config_name="production"):
     mail.init_app(app)
 
     from . import models
+
     @login_manager.user_loader
     def load_user(user_id):
         return models.User.query.get(int(user_id))
@@ -47,22 +49,22 @@ def create_app(config_name="production"):
     @app.shell_context_processor
     def shell_context():
         return dict(
-            db = db,
-            User = models.User,
-            Role = models.Role,
-            PersonalData = models.PersonalData,
-            Author = models.Author,
-            Book = models.Book,
-            Borrow = models.Borrow
+            db=db,
+            User=models.User,
+            Role=models.Role,
+            PersonalData=models.PersonalData,
+            Author=models.Author,
+            Book=models.Book,
+            Borrow=models.Borrow,
         )
 
     from .cli import init_db
     from .cli import insert_test_data
     from .cli import test
+
     app.cli.add_command(init_db)
     app.cli.add_command(insert_test_data)
     app.cli.add_command(test)
-
 
     from .auth import auth
     from .main import main
@@ -75,3 +77,8 @@ def create_app(config_name="production"):
     app.register_blueprint(api)
 
     return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
